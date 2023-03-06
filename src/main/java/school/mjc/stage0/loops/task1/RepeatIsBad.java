ba061e69c388be7c1e9bad3482a9deafb36231ca
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-03-06 22:13:02 +0200

    Update RepeatIsBad.java

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -4,6 +4,33 @@ public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
+        System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
 
     }
+    public static void main (String[] args){
+     RepeatIsBad main = new RepeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        main.repeatIsBad();
+        
+        
+    }
+    
 }
